6e5de3144a98f0950238b6fb801d4c255deb14e7
eun0leee <eun0leee@gmail.com>
2023-02-13 16:18:51 +0900

    Feat : 헤더 작업

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,40 @@
-import { GlobalStyle } from "../App"
+import { Link } from 'react-router-dom';
+import { FaUserAlt } from 'react-icons/fa';
+import { AiFillHeart } from 'react-icons/ai';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
     <>
-      <GlobalStyle />
-      <header>header</header>
+      <HeaderComponents>
+        <StlyedLink to={'/wish'}>
+          <AiFillHeart size="24" color="green" />
+        </StlyedLink>
+        <StlyedLink to={'/all'}>
+          <FaUserAlt size="24" color="green" />
+        </StlyedLink>
+      </HeaderComponents>
     </>
-  )
-}
+  );
+};
 
-export default Header
+const HeaderComponents = styled.header`
+  background-color: #d7d5d5;
+  padding: 0;
+  position: fixed;
+  height: 70px;
+  width: 100%;
+  top: 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const StlyedLink = styled(Link)`
+  width: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Header;
