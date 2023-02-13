56128f3504eb88e2a30b56f6a85110eecf5c61dd
Hyojina <hjkim3009@gmail.com>
2023-02-13 21:43:48 +0900

    Feat: 라우터 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App'
+import App from './App';
 import Error from './components/Error';
 import Main from './pages/main/mainPage';
 import Test from './pages/TestPage';
+import Intro from './pages/intro/Intro';
+import Login from './pages/login/Login';
+import SignUp from './pages/signUp/SignUp';
+import MyPage from './pages/myPage/MyPage';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -16,10 +20,14 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { index: true, element: <Main /> },
-      { path: '/test', element: <Test /> }
-    ]
-  }
-])
+      { path: '/test', element: <Test /> },
+      { path: '/intro', element: <Intro /> },
+      { path: '/login', element: <Login /> },
+      { path: '/signup', element: <SignUp /> },
+      { path: '/mypage', element: <MyPage /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -27,4 +35,4 @@ root.render(
     <RouterProvider router={router} />
     <ReactQueryDevtools initialIsOpen={true} />
   </QueryClientProvider>
-)
+);
