38173614f2cbcc9a8ac75617413c124b8fdcb36a
eun0leee <eun0leee@gmail.com>
2023-02-13 15:30:34 +0900

    Feat : 검색 라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App'
+import App from './App';
 import Error from './components/Error';
 import Main from './pages/main/mainPage';
 import Test from './pages/TestPage';
+import Search from './pages/search/searchPage';
+import ResultsTotal from './pages/search/resultsTotalPage';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -16,10 +18,12 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { index: true, element: <Main /> },
-      { path: '/test', element: <Test /> }
-    ]
-  }
-])
+      { path: '/test', element: <Test /> },
+      { path: '/search', element: <Search /> },
+      { path: '/search/:keywords/:category', element: <ResultsTotal /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -27,4 +31,4 @@ root.render(
     <RouterProvider router={router} />
     <ReactQueryDevtools initialIsOpen={true} />
   </QueryClientProvider>
-)
+);
