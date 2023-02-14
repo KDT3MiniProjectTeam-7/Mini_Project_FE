e1b496ee6435ea5a18b045c03dd4ca96af4cbcd4
Hyojina <hjkim3009@gmail.com>
2023-02-14 14:12:58 +0900

    Fix: 머지 충돌 해결

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,12 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import Error from './components/Error';
 import Main from './pages/main/mainPage';
-<<<<<<< HEAD
 import Test from './pages/TestPage';
 import Intro from './pages/intro/Intro';
 import Login from './pages/login/Login';
 import SignUp from './pages/signUp/SignUp';
 import MyPage from './pages/myPage/MyPage';
-=======
 import ALL from './pages/all/allPage';
->>>>>>> 12700f3d5ae7a2fadd21bd4ca3ac15b7e5ea99ca
 
 const queryClient = new QueryClient();
 
@@ -24,15 +21,12 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { index: true, element: <Main /> },
-<<<<<<< HEAD
       { path: '/test', element: <Test /> },
       { path: '/intro', element: <Intro /> },
       { path: '/login', element: <Login /> },
       { path: '/signup', element: <SignUp /> },
       { path: '/mypage', element: <MyPage /> },
-=======
       { path: '/all', element: <ALL /> },
->>>>>>> 12700f3d5ae7a2fadd21bd4ca3ac15b7e5ea99ca
     ],
   },
 ]);
