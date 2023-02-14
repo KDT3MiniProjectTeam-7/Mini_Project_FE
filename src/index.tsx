69a9784a784d1d7477943f10a7a59864ad3d12ab
eun0leee <eun0leee@gmail.com>
2023-02-14 20:59:06 +0900

    Fix : 병합 충돌 해결

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import Error from './components/Error';
 
-import Main from './pages/main/mainPage';
+import Main from './pages/main/MainPage';
 import Intro from './pages/intro/Intro';
 import Login from './pages/login/Login';
 import SignUp from './pages/signUp/SignUp';
 import MyPage from './pages/myPage/MyPage';
+import Search from './pages/search/SearchPage';
 import ALL from './pages/all/allPage';
-import Search from './pages/search/searchPage';
-import ResultsTotal from './pages/search/resultsTotalPage';
+import ResultsTotal from './pages/search/ResultsTotalPage';
 import Survey from './pages/survey/Survey';
 import Detail from './pages/detail/detailPage';
 
