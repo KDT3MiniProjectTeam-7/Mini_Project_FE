6f3f19896a1d036d7bc59a06b07ed1829adb1091
JYROH <ghgt1@naver.com>
2023-02-15 18:06:06 +0900

    Feat: 4가지 카테고리 마크업 완료

--- src/pages/detail/Loan.tsx
+++ src/pages/detail/Loan.tsx
@@ -6,36 +6,43 @@ import DescriptionData from './DescriptionData';
 
 const Loan = () => {
   const [nav, setNav] = useState([true, false, false]);
+
   const data = {
-    category: 'loan',
-    productTitle: '모바일 프라임론 직장인 대출',
-    bankName: '광주은행',
-    productImageUrl:
-      'https://mblogthumb-phinf.pstatic.net/MjAyMTAxMjRfMTg2/MDAxNjExNDcyNDQwNjMz.c4nA0jGYMPxo1ZkOCb9DxtC_pMoZJWo14c_VsdXbeuIg.3XJxeZziXV4TUIowr4dAJBJMY0xmVTIchgzH56o_C3sg.PNG.yeosu_bada/%EA%B4%91%EC%A3%BC%EC%9D%80%ED%96%89_%EB%A1%9C%EA%B3%A0_PNG_AI_%EC%9B%90%EB%B3%B8_%EB%8B%A4%EC%9A%B4.png?type=w800',
-    applicationUrl:
-      'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&pkid=731&os=29654553&qvt=0&query=%EA%B4%91%EC%A3%BC%EC%9D%80%ED%96%89%20%EB%AA%A8%EB%B0%94%EC%9D%BC%ED%94%84%EB%9D%BC%EC%9E%84%EB%A1%A0',
-    lowestRates: 6.91,
-    highestRates: 12.71,
-    limits: '1백만원 ~ 1억 5천만원',
+    productId: 18,
+    category: 'LOAN',
+    productName: '신용대출',
+    companyName: '케이뱅크',
+    companyImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Kbank_logo.svg/2560px-Kbank_logo.svg.png',
+    productURL:
+      'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&pkid=731&os=29654540&qvt=0&query=%EC%BC%80%EC%9D%B4%EB%B1%85%ED%81%AC%20%EC%8B%A0%EC%9A%A9%EB%8C%80%EC%B6%9C',
+    thumbnail: null,
+    lowRate: '6.44',
+    highRate: '10.49',
+    bound: ['연소득의 150% 범위내', '최대 2억원'],
+    qualification: ['직장인'],
   };
 
   return (
     <>
       <IntroContainer>
-        <p>{data.bankName}</p>
-        <img src={data.productImageUrl} alt="cardImage" />
-        <h1>{data.productTitle}</h1>
+        <p>{data.companyName}</p>
+        <img src={data.companyImage} alt="cardImage" />
+        <h1>{data.productName}</h1>
         <h2>파이낸스 세븐이 추천하는 최적의 대출</h2>
       </IntroContainer>
       <BenefitContainer>
         <li>
           <FaAngellist />
-          <span>{`최저 ${data.lowestRates}%, 최고 ${data.highestRates}% 의 금리`}</span>
+          <span>{`최저 ${data.lowRate}%, 최고 ${data.highRate}% 의 금리`}</span>
         </li>
-        <li>
-          <FaAngellist />
-          <span>{`${data.limits}의 한도`}</span>
-        </li>
+        {data.bound.map((bound, index) => {
+          return (
+            <li key={index}>
+              <FaAngellist />
+              <span>{bound}</span>
+            </li>
+          );
+        })}
       </BenefitContainer>
       {/* isSelected를 통해서 select된 nav는 다른 스타일 부여 */}
       <IndexContainer>
@@ -53,13 +60,19 @@ const Loan = () => {
         <DescriptionContainer>
           <p>금리정보</p>
           <ul>
-            <li>{`· 최저금리(연체이자율 포함): 연 ${data.lowestRates}%`}</li>
-            <li>{`· 최고금리(연체이자율 포함): 연 ${data.highestRates}%`}</li>
+            <li>{`· 최저금리(연체이자율 포함): 연 ${data.lowRate}%`}</li>
+            <li>{`· 최고금리(연체이자율 포함): 연 ${data.highRate}%`}</li>
             <li>{`· 연체이자율 : 적용금리 + 연 3%(최고 연 12%)`}</li>
           </ul>
           <p>한도</p>
           <ul>
-            <li>{`· ${data.limits}`}</li>
+            {data.bound.map((bound, index) => {
+              return (
+                <li key={index}>
+                  <span>{`· ${bound}`}</span>
+                </li>
+              );
+            })}
           </ul>
         </DescriptionContainer>
       )}
@@ -68,12 +81,12 @@ const Loan = () => {
           <p>우대금리</p>
           <ul>
             <li>
-              {`· ${data.bankName} 계좌로 매월 급여이체 : 연 0.3%p 대량 급여이체/급여지정일 입금/회사명의 입금 등을 통해 매월 100만원 이상의 급여가 입금될 경우 실적인정 적용`}
+              {`· ${data.companyName} 계좌로 매월 급여이체 : 연 0.3%p 대량 급여이체/급여지정일 입금/회사명의 입금 등을 통해 매월 100만원 이상의 급여가 입금될 경우 실적인정 적용`}
             </li>
             <li>· 신용카드 당행계좌 결제 실적(매월 30만원 이상) : 연 0.2%p</li>
             <li>· WON뱅킹 매월 1회 이상 로그인 : 연 0.2%p</li>
             <li>
-              {`· 대출 미보유 고객 우대금리 쿠폰 : 연 0.3%p 신청일 현재 ${data.bankName} 대출이 없는 고객(1회 발급/사용 가능하며, 최초 약정기간만 적용하고 연장 시는 적용
+              {`· 대출 미보유 고객 우대금리 쿠폰 : 연 0.3%p 신청일 현재 ${data.companyName} 대출이 없는 고객(1회 발급/사용 가능하며, 최초 약정기간만 적용하고 연장 시는 적용
 불가)`}
             </li>
           </ul>
@@ -85,7 +98,7 @@ const Loan = () => {
           <DescriptionData />
         </DescriptionContainer>
       )}
-      <Link to={data.applicationUrl}>
+      <Link to={data.productURL}>
         <SubmitButton>온라인 신청</SubmitButton>
       </Link>
     </>
