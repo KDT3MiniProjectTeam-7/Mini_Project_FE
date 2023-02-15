f8c036cf0f918b571fc73fbe77a398f0edf00191
eun0leee <eun0leee@gmail.com>
2023-02-15 23:09:10 +0900

    Feat : 검색결과_카테고리별 탭 전환

--- src/pages/search/SearchResultsPage.tsx
+++ src/pages/search/SearchResultsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import styled from 'styled-components';
 import SearchBox from './SearchResultsComponents/SearchBox';
 import ResultsTotal from './SearchComponents/ResultsTotal';
@@ -18,23 +18,46 @@ const SearchResults = () => {
     { title: '청약', content: <ResultsSubscription /> },
   ];
 
-  const selectCategory = (index: number) => {
-    setCategory(index);
-  };
-
   return (
     <>
       <Container>
         <SearchBox />
-        <CategoryTab>{categoryArr[category].title}</CategoryTab>
+        <CategoryTab>
+          {categoryArr.map((list, index) => (
+            <li key={list.title} className={index === category ? 'clicked' : ''} onClick={() => setCategory(index)}>
+              {list.title}
+            </li>
+          ))}
+        </CategoryTab>
         <CategoryDesc>{categoryArr[category].content}</CategoryDesc>
       </Container>
     </>
   );
 };
 
-const Container = styled.main``;
-const CategoryTab = styled.ol``;
+const Container = styled.main`
+  margin-top: 125px;
+`;
+const CategoryTab = styled.ol`
+  display: flex;
+  justify-content: space-between;
+  gap: 13px;
+  border-bottom: 1px solid #ebebeb;
+
+  font-size: 14px;
+  font-weight: 500;
+
+  li {
+    padding: 12px 17px;
+    color: #5b5c5e;
+    transition: all 0.3ms ease-in;
+
+    &.clicked {
+      color: #0e76ff;
+      border-bottom: 3px solid #0e76ff;
+    }
+  }
+`;
 const CategoryDesc = styled.div``;
 
 export default SearchResults;
