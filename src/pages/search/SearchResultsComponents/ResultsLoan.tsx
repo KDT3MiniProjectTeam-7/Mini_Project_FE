1f146137f8b8468732658ee9ce5df4654fb4c989
eun0leee <eun0leee@gmail.com>
2023-02-16 22:25:30 +0900

    Design : 검색결과 대출_스타일링

--- src/pages/search/SearchResultsComponents/ResultsLoan.tsx
+++ src/pages/search/SearchResultsComponents/ResultsLoan.tsx
@@ -39,7 +39,7 @@ const ResultsLoan = () => {
     {
       productId: 2114,
       category: 'loan',
-      productName: 'KB국민 개인사업자대출',
+      productName: '개인사업자대출',
       companyName: 'KB국민카드',
       companyImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/KB_logo.svg/2560px-KB_logo.svg.png',
       lowRate: 7.4,
@@ -48,6 +48,59 @@ const ResultsLoan = () => {
     },
     {
       productId: 2115,
+      category: 'loan',
+      productName: '신용대출플러스',
+      companyName: '케이뱅크',
+      companyImage:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Kbank_logo.svg/2560px-Kbank_logo.svg.png',
+      lowRate: 7.35,
+      highRate: 10.86,
+      bound: '최대 1억 5천원',
+    },
+    {
+      productId: 2116,
+      category: 'loan',
+      productName: '신용대출',
+      companyName: '케이뱅크',
+      companyImage:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Kbank_logo.svg/2560px-Kbank_logo.svg.png',
+      lowRate: 6.44,
+      highRate: 10.49,
+      bound: '최대 2억원',
+    },
+    {
+      productId: 2117,
+      category: 'loan',
+      productName: '모바일 프라임론 직장인 대출',
+      companyName: '광주은행',
+      companyImage:
+        'https://mblogthumb-phinf.pstatic.net/MjAyMTAxMjRfMTg2/MDAxNjExNDcyNDQwNjMz.c4nA0jGYMPxo1ZkOCb9DxtC_pMoZJWo14c_VsdXbeuIg.3XJxeZziXV4TUIowr4dAJBJMY0xmVTIchgzH56o_C3sg.PNG.yeosu_bada/%EA%B4%91%EC%A3%BC%EC%9D%80%ED%96%89_%EB%A1%9C%EA%B3%A0_PNG_AI_%EC%9B%90%EB%B3%B8_%EB%8B%A4%EC%9A%B4.png?type=w800',
+      lowRate: 6.91,
+      highRate: 12.71,
+      bound: '1백만 원 ~ 1억 5천만 원',
+    },
+    {
+      productId: 2118,
+      category: 'loan',
+      productName: '사장님대출',
+      companyName: '토스뱅크',
+      companyImage: 'https://static.toss.im/logos/png/4x/logo-bank.png',
+      lowRate: 5.99,
+      highRate: 15,
+      bound: '1백만원 ~ 1억원',
+    },
+    {
+      productId: 2119,
+      category: 'loan',
+      productName: '개인사업자대출',
+      companyName: 'KB국민카드',
+      companyImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/KB_logo.svg/2560px-KB_logo.svg.png',
+      lowRate: 7.4,
+      highRate: 19.9,
+      bound: '1백만원 ~ 2.5억원',
+    },
+    {
+      productId: 21110,
       category: 'loan',
       productName: '신용대출플러스',
       companyName: '케이뱅크',
@@ -72,24 +125,79 @@ const ResultsLoan = () => {
               <img src={list.companyImage} alt={`${list.companyName} 로고`} />
             </CompanyImage>
             <Desc>
-              <dt className="companyName">{list.companyName}</dt>
-              <dd className="productname">{list.productName}</dd>
-              <dd>최저 {list.lowRate}%</dd>
+              <h3 className="companyName">{list.companyName}</h3>
+              <p className="productname">{list.productName}</p>
             </Desc>
           </div>
+          <Rate>최저 연 {list.lowRate}%</Rate>
         </li>
       ))}
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 30px 20px;
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+
+    > div {
+      display: flex;
+    }
+
+    & + li {
+      margin-top: 40px;
+    }
+  }
+`;
+
 const CompanyImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  min-width: 50px;
+  height: 40px;
+  margin-right: 20px;
+
   img {
-    max-width: 40px;
+    max-width: 50px;
     max-height: 40px;
   }
 `;
-const Desc = styled.dl``;
+
+const Desc = styled.dl`
+  display: flex;
+  flex-direction: column;
+
+  margin-right: 20px;
+
+  .productname {
+    order: 1;
+    margin-bottom: 5px;
+    color: #5e6675;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 1.4;
+  }
+
+  .companyName {
+    order: 2;
+    color: #676f7b;
+    font-size: 10.5px;
+    font-weight: 500;
+  }
+`;
+
+const Rate = styled.strong`
+  width: 120px;
+  color: #4880ee;
+  font-weight: 700;
+  font-size: 14px;
+  text-align: right;
+`;
 
 export default ResultsLoan;
