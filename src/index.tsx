562f29ca8e9fca7fb85f74182d7a9cfef92c2dd7
JYROH <ghgt1@naver.com>
2023-02-16 18:41:36 +0900

    Feat: index에 Redux 프로바이더 삽입

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux';
 import App from './App';
 import Error from './components/Error';
-
 import Main from './pages/main/MainPage';
 import Intro from './pages/intro/Intro';
 import Login from './pages/login/Login';
@@ -15,6 +15,8 @@ import ALL from './pages/all/allPage';
 import SearchResults from './pages/search/SearchResultsPage';
 import Survey from './pages/survey/Survey';
 import Detail from './pages/detail/detailPage';
+import Cart from './pages/cart/CartPage';
+import store from './store/store';
 
 const queryClient = new QueryClient();
 
@@ -34,6 +36,7 @@ const router = createBrowserRouter([
       { path: '/survey', element: <Survey /> },
       { path: '/all', element: <ALL /> },
       { path: '/detail/:category/:id', element: <Detail /> },
+      { path: '/cart', element: <Cart /> },
     ],
   },
 ]);
@@ -41,7 +44,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
     <ReactQueryDevtools initialIsOpen={true} />
   </QueryClientProvider>
 );
