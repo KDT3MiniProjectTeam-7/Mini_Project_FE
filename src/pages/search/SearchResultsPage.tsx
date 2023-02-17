dc729ce177ff2cadda01bf9c8d9531f9617da70d
eun0leee <eun0leee@gmail.com>
2023-02-17 20:50:23 +0900

    Style : 카테고리탭 배열 state에서 분리

--- src/pages/search/SearchResultsPage.tsx
+++ src/pages/search/SearchResultsPage.tsx
@@ -8,21 +8,23 @@ import ResultsLoan from './SearchResultsComponents/ResultsLoan';
 import ResultsSavings from './SearchResultsComponents/ResultsSavings';
 import ResultsSubscription from './SearchResultsComponents/ResultsSubscription';
 
+const categoryArr = [
+  { category: 'total', title: '통합', content: <ResultsTotal /> },
+  { category: 'card', title: '카드', content: <ResultsCard /> },
+  { category: 'loan', title: '대출', content: <ResultsLoan /> },
+  { category: 'savings', title: '예적금', content: <ResultsSavings /> },
+  { category: 'subscription', title: '청약', content: <ResultsSubscription /> },
+];
+
 const SearchResults = () => {
   const [tabIndex, setTabIndex] = useState(0);
-  const [categoryArr, setCategoryArr] = useState([
-    { category: 'total', title: '통합', content: <ResultsTotal /> },
-    { category: 'card', title: '카드', content: <ResultsCard /> },
-    { category: 'loan', title: '대출', content: <ResultsLoan /> },
-    { category: 'savings', title: '예적금', content: <ResultsSavings /> },
-    { category: 'subscription', title: '청약', content: <ResultsSubscription /> },
-  ]);
+  const [category, setCategory] = useState(categoryArr);
 
   return (
     <Container>
       <SearchBox />
-      <CategoryTab tabIndex={tabIndex} setTabIndex={setTabIndex} categoryArr={categoryArr} />
-      <CategoryDesc>{categoryArr[tabIndex].content}</CategoryDesc>
+      <CategoryTab tabIndex={tabIndex} setTabIndex={setTabIndex} categoryArr={category} />
+      <CategoryDesc>{category[tabIndex].content}</CategoryDesc>
     </Container>
   );
 };
