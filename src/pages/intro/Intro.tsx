a5eb9f7839dd359589cd03cc7a6a612572331f1f
Hyojina <hjkim3009@gmail.com>
2023-02-20 03:09:47 +0900

    Design: 접속 페이지 디자인 변경
    - 디자인 변경
      - Style.tsx 파일에서 컴포넌트 분리
      - 사용자 지정 CSS 적용

--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -1,18 +1,40 @@
 import styled from 'styled-components';
-import { Container, LinkForm as Link, LogoForm } from '../../common/style/Style';
+import { Link as LinkForm } from 'react-router-dom';
 
 const Intro = () => {
   return (
-    <Container>
+    <main>
       <Logo />
       <Link to={'/login'}>로그인</Link>
       <Link to={'/signup'}>회원가입</Link>
-    </Container>
+    </main>
   );
 };
 
-const Logo = styled(LogoForm).attrs({
+const Logo = styled.img.attrs({
   src: 'https://compphoto.incruit.com/2021/12/FCCI2.png',
-})``;
+  alt: '로고 이미지',
+})`
+  width: 100%;
+  max-width: 400px;
+`;
+
+const Link = styled(LinkForm)`
+  width: 100%;
+  background-color: var(--lightgray-color);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: var(--input-height);
+  font-size: var(--font-m);
+  font-weight: bold;
+  text-decoration: none;
+  border-radius: 20px;
+  margin: 10px 0;
+
+  :visited {
+    color: var(--black-color);
+  }
+`;
 
 export default Intro;
