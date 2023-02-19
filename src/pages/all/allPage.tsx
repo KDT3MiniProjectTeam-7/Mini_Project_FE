e0c2028f294a8506f7c955c1235fde9dfb9a1ef7
JYROH <ghgt1@naver.com>
2023-02-19 12:50:24 +0900

    Feat: 모아보기 페이지 카테고리 추가

--- src/pages/all/allPage.tsx
+++ src/pages/all/allPage.tsx
@@ -227,6 +227,75 @@ const ALL = () => {
     },
   ];
 
+  const savingsData = [
+    {
+      productId: 3111,
+      category: 'savings',
+      productName: '청년내일저축계좌',
+      companyName: '하나은행',
+      companyImage:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Hana_Bank_Logo_(kor).svg/2560px-Hana_Bank_Logo_(kor).svg.png',
+      basicRate: 2.0,
+      primeRate: 3.0,
+    },
+    {
+      productId: 3112,
+      category: 'savings',
+      productName: '스무살 우리 자유적금',
+      companyName: '우리은행',
+      companyImage: 'https://simg.wooribank.com/img/intro/header/h1_01.png',
+      basicRate: 2.8,
+      primeRate: 3.9,
+    },
+    {
+      productId: 3113,
+      category: 'savings',
+      productName: '직장인우대예금',
+      companyName: 'DGB대구은행',
+      companyImage: 'https://ifh.cc/g/tN8fPq.png',
+      basicRate: 3.66,
+      primeRate: 4.21,
+    },
+    {
+      productId: 3114,
+      category: 'savings',
+      productName: '직장인우대예금',
+      companyName: 'DGB대구은행',
+      companyImage:
+        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdLrsWe%2FbtrGI0Sscvk%2FOWqTiEq6Q8JIVZSBHjYHcK%2Fimg.png',
+      basicRate: 3.26,
+      primeRate: 3.81,
+    },
+    {
+      productId: 3115,
+      category: 'savings',
+      productName: 'IBK평생한가족통장',
+      companyName: 'IBK기업은행',
+      companyImage: 'https://blog.kakaocdn.net/dn/tUFqP/btq5QOAMjv3/pQgStk8N7muGHKPj7Ph600/img.png',
+      basicRate: 3.45,
+      primeRate: 3.65,
+    },
+  ];
+
+  const subscriptionData = [
+    {
+      productId: 4111,
+      category: 'subscription',
+      productName: '청년 우대형 주택청약종합저축',
+      companyName: '우리은행',
+      companyImage: 'https://simg.wooribank.com/img/intro/header/h1_01.png',
+      highRate: 2.1,
+    },
+    {
+      productId: 4112,
+      category: 'subscription',
+      productName: '주택청약종합저축',
+      companyName: '신한은행',
+      companyImage: 'https://simg.wooribank.com/img/intro/header/h1_01.png',
+      highRate: 3.6,
+    },
+  ];
+
   let ItemLists = <></>;
   switch (tabIndex) {
     case 0:
@@ -235,12 +304,12 @@ const ALL = () => {
     case 1:
       ItemLists = <LoanLists data={loanData} />;
       break;
-    // case 2:
-    //   ItemLists = <SavingsLists data={data} />;
-    //   break;
-    // case 3:
-    //   ItemLists = <SubscriptionLists data={data} />;
-    //   break;
+    case 2:
+      ItemLists = <SavingsLists data={savingsData} />;
+      break;
+    case 3:
+      ItemLists = <SubscriptionLists data={subscriptionData} />;
+      break;
     default:
       alert('카테고리를 표시해주세요.');
   }
