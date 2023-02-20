98025f2e751e31facd98beec5a8bdb4d4b5bbeec
eun0leee <eun0leee@gmail.com>
2023-02-21 00:02:16 +0900

    Feat : api 추가

--- src/common/api/Api.tsx
+++ src/common/api/Api.tsx
@@ -26,9 +26,10 @@ export const getDataTest = async () => {
 // 최근 검색어
 export const getSearchKeywords = async () => {
   try {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
     const data = await axios.get('http://3.36.178.242:8080/user/keywords');
-    console.log(data);
-    return data;
+    // console.log(data.data.resultData);
+    return data.data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
@@ -36,9 +37,10 @@ export const getSearchKeywords = async () => {
 
 export const addSearchKeywords = async (keywords: string) => {
   try {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
     const data = await axios.post('http://3.36.178.242:8080/user/keywords', { searchContent: keywords });
-    console.log(data);
-    return data;
+    // console.log(data.data.resultData);
+    return data.data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
@@ -46,11 +48,10 @@ export const addSearchKeywords = async (keywords: string) => {
 
 export const deleteSearchKeywordsSingle = async (searchId: number) => {
   try {
-    const data = await axios.delete('http://3.36.178.242:8080/user/keywords', {
-      data: { searchId: `${searchId}` },
-    });
-    console.log(data);
-    return data;
+    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
+    const data = await axios.delete('http://3.36.178.242:8080/user/keywords', { searchId: searchId });
+    // console.log(data.data.resultData);
+    return data.data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
@@ -58,9 +59,44 @@ export const deleteSearchKeywordsSingle = async (searchId: number) => {
 
 export const deleteSearchKeywordsAll = async (keywords: string) => {
   try {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
     const data = await axios.delete('http://3.36.178.242:8080/user/keywords/all');
-    console.log(data);
-    return data;
+    // console.log(data.data.resultData);
+    return data.data.resultData;
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
+
+// 최근 본 상품
+export const getRecentProduct = async () => {
+  try {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
+    const data = await axios.get('http://3.36.178.242:8080/user/recentproducts');
+    // console.log(data.data.resultData);
+    return data.data.resultDatata;
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
+
+export const addRecentProduct = async (productId: number) => {
+  try {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
+    const data = await axios.post('http://3.36.178.242:8080/user/recentproducts', { productId: productId });
+    // console.log(data.data.resultData);
+    return data.data.resultData;
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
+
+// 검색결과 조회
+export const getSearchResults = async (title: string, category: string, page: number) => {
+  try {
+    const data = await axios.get(`http://3.36.178.242:8080/search?title=${title}&category=${category}&page=${page}`);
+    console.log(data.data.resultData);
+    return data.data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
