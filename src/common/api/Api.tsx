306e07bc8aa3af54f858b22fc70ba628c760ba77
eun0leee <eun0leee@gmail.com>
2023-02-20 23:01:32 +0900

    Feat : 최근검색어 api 작성

--- src/common/api/Api.tsx
+++ src/common/api/Api.tsx
@@ -22,3 +22,46 @@ export const getDataTest = async () => {
     console.log(err.message);
   }
 };
+
+// 최근 검색어
+export const getSearchKeywords = async () => {
+  try {
+    const data = await axios.get('http://3.36.178.242:8080/user/keywords');
+    console.log(data);
+    return data;
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
+
+export const addSearchKeywords = async (keywords: string) => {
+  try {
+    const data = await axios.post('http://3.36.178.242:8080/user/keywords', { searchContent: keywords });
+    console.log(data);
+    return data;
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
+
+export const deleteSearchKeywordsSingle = async (searchId: number) => {
+  try {
+    const data = await axios.delete('http://3.36.178.242:8080/user/keywords', {
+      data: { searchId: `${searchId}` },
+    });
+    console.log(data);
+    return data;
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
+
+export const deleteSearchKeywordsAll = async (keywords: string) => {
+  try {
+    const data = await axios.delete('http://3.36.178.242:8080/user/keywords/all');
+    console.log(data);
+    return data;
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
