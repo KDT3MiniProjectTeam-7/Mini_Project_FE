6a85dab22d971efda7d984fd1efb1170399e5766
eun0leee <eun0leee@gmail.com>
2023-02-21 15:53:01 +0900

    Comment : 주석 제거

--- src/common/api/Api.tsx
+++ src/common/api/Api.tsx
@@ -49,14 +49,11 @@ export const delCartItems = async (id: number) => {
 export const getSearchKeywords = async () => {
   try {
     axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
-    // const data = await axios.get(
-    //   'https://7102b765-02ea-4b41-983b-addf4c37adea.mock.pstmn.io/user/recent-products/user/keywords'
-    // );
     const data = await axios.get('http://3.36.178.242:8080/user/keywords');
-    // console.log(data.data.resultData);
+    console.log(data.data.resultData);
     return data.data.resultData;
   } catch (err: any) {
-    // console.log(err.message);
+    console.log(err.message);
   }
 };
 
@@ -64,10 +61,10 @@ export const addSearchKeywords = async (keywords: string) => {
   try {
     axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
     const data = await axios.post('http://3.36.178.242:8080/user/keywords', { searchContent: keywords });
-    // console.log(data.data.resultData);
+    console.log(data.data.resultData);
     return data.data.resultData;
   } catch (err: any) {
-    // console.log(err.message);
+    console.log(err.message);
   }
 };
 
@@ -75,11 +72,11 @@ export const deleteSearchKeywordsSingle = async (searchId: number) => {
   try {
     axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
     const data = await axios.delete('http://3.36.178.242:8080/user/keywords', { data: { searchId: searchId } });
-    // console.log(data.data.resultData);
+    console.log(data.data.resultData);
     console.log('한개 삭제됨!');
     return data.data.resultData;
   } catch (err: any) {
-    // console.log(err.message);
+    console.log(err.message);
   }
 };
 
@@ -87,10 +84,10 @@ export const deleteSearchKeywordsAll = async () => {
   try {
     axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
     const data = await axios.delete('http://3.36.178.242:8080/user/keywords/all');
-    // console.log(data.data.resultData);
+    console.log(data.data.resultData);
     return data.data.resultData;
   } catch (err: any) {
-    // console.log(err.message);
+    console.log(err.message);
   }
 };
 
@@ -99,21 +96,22 @@ export const getRecentProduct = async () => {
   try {
     axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
     const data = await axios.get('http://3.36.178.242:8080/user/recentproducts');
-    // console.log(data.data.resultData);
+    console.log(data.data.resultData);
     return data.data.resultDatata;
   } catch (err: any) {
-    // console.log(err.message);
+    console.log(err.message);
   }
 };
 
 export const addRecentProduct = async (productId: number) => {
   try {
     axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie}`;
+    console.log(document.cookie);
     const data = await axios.post('http://3.36.178.242:8080/user/recentproducts', { productId: productId });
-    // console.log(data.data.resultData);
+    console.log(data.data.resultData);
     return data.data.resultData;
   } catch (err: any) {
-    // console.log(err.message);
+    console.log(err.message);
   }
 };
 
@@ -121,9 +119,9 @@ export const addRecentProduct = async (productId: number) => {
 export const getSearchResults = async (title: string, category: string, page: number) => {
   try {
     const data = await axios.get(`http://3.36.178.242:8080/search?title=${title}&category=${category}&page=${page}`);
-    console.log(data.data.resultData);
+    console.log(data);
     return data.data.resultData;
   } catch (err: any) {
-    // console.log(err.message);
+    console.log(err.message);
   }
 };
