21240d8aec480557d76490f0995209f9a5020ce1
eun0leee <eun0leee@gmail.com>
2023-02-22 17:30:23 +0900

    Feat : axios 인스턴스 적용

--- src/common/api/Api.tsx
+++ src/common/api/Api.tsx
@@ -22,88 +22,11 @@ export const delCartItems = async (id: number) => {
   }
 };
 
-////////////////////// 검색 ///////////////////
-//최근 검색어
-// export const getSearchKeywords = async () => {
-//   try {
-//     const { data } = await authInstance.get('/keywords');
-//     console.log(data.data.resultData);
-//     return data.data.resultData;
-//   } catch (err: any) {
-//     console.log(err.message);
-//   }
-// };
-
-// export const addSearchKeywords = async (keywords: string) => {
-//   try {
-//     const { data } = await authInstance.post('/user/keywords', { data: { searchContent: keywords } });
-//     console.log(data);
-//     return data.data.resultData;
-//   } catch (err: any) {
-//     console.log(err.message);
-//   }
-// };
-
-// export const deleteSearchKeywordsSingle = async (searchId: number) => {
-//   try {
-//     const { data } = await authInstance.delete('/user/keywords', { data: { searchId: searchId } });
-//     console.log(data.data.resultData);
-//     console.log('한개 삭제됨!');
-//     return data.data.resultData;
-//   } catch (err: any) {
-//     console.log(err.message);
-//   }
-// };
-
-// export const deleteSearchKeywordsAll = async () => {
-//   try {
-//     const { data } = await authInstance.delete('/user/keywords/all');
-//     console.log(data.data.resultData);
-//     return data.data.resultData;
-//   } catch (err: any) {
-//     console.log(err.message);
-//   }
-// };
-
-// // 최근 본 상품
-// export const getRecentProduct = async () => {
-//   try {
-//     const { data } = await authInstance.get('/user/recentproducts');
-//     console.log(data.data.resultData);
-//     return data.data.resultDatata;
-//   } catch (err: any) {
-//     console.log(err.message);
-//   }
-// };
-
-// export const addRecentProduct = async (productId: number) => {
-//   try {
-//     const { data } = await authInstance.post('/user/recentproducts', { data: { productId: productId } });
-//     console.log(data);
-//     return data.data.resultData;
-//   } catch (err: any) {
-//     console.log(err.message);
-//   }
-// };
-
-// // 검색결과 조회
-// export const getSearchResults = async (title: string, category: string, page: number) => {
-//   try {
-//     const { data } = await defaultInstance.get(`/search?title=${title}&category=${category}&page=${page}`);
-//     console.log(data.data.resultData);
-//     return data.data.resultData;
-//   } catch (err: any) {
-//     console.log(err.message);
-//   }
-// };
-
 // 최근 검색어
 export const getSearchKeywords = async () => {
   try {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.slice(12)}`;
-    const data = await axios.get('http://finance-seven.store/user/keywords');
-    // console.log(data.data.resultData);
-    return data.data.resultData;
+    const { data } = await authInstance.get('/user/keywords');
+    return data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
@@ -111,11 +34,8 @@ export const getSearchKeywords = async () => {
 
 export const addSearchKeywords = async (keywords: string) => {
   try {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.slice(12)}`;
-    const data = await axios.post('http://finance-seven.store/user/keywords', { searchContent: keywords });
-    // console.log(data);
+    await authInstance.post('/user/keywords', { searchContent: keywords });
     console.log('검색어 추가 완료');
-    return data.data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
@@ -123,11 +43,7 @@ export const addSearchKeywords = async (keywords: string) => {
 
 export const deleteSearchKeywordsSingle = async (searchId: number) => {
   try {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.slice(12)}`;
-    const data = await axios.delete('http://finance-seven.store/user/keywords', { data: { searchId: searchId } });
-    // console.log(data);
-    console.log('한개 삭제됨!');
-    return data;
+    await authInstance.delete('/user/keywords', { data: { searchId: searchId } });
   } catch (err: any) {
     console.log(err.message);
   }
@@ -135,10 +51,8 @@ export const deleteSearchKeywordsSingle = async (searchId: number) => {
 
 export const deleteSearchKeywordsAll = async () => {
   try {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.slice(12)}`;
-    const data = await axios.delete('http://finance-seven.store/user/keywords/all');
-    console.log('전체삭제됨!');
-    return data.data;
+    const { data } = await authInstance.delete('/user/keywords/all');
+    return data;
   } catch (err: any) {
     console.log(err.message);
   }
@@ -147,9 +61,8 @@ export const deleteSearchKeywordsAll = async () => {
 // 최근 본 상품
 export const getRecentProduct = async () => {
   try {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.slice(12)}`;
-    const data = await axios.get('http://finance-seven.store/user/recentproducts');
-    return data.data.resultData;
+    const { data } = await authInstance.get('/user/recentproducts');
+    return data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
@@ -157,11 +70,8 @@ export const getRecentProduct = async () => {
 
 export const addRecentProduct = async (productId: number) => {
   try {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.slice(12)}`;
-    const data = await axios.post('http://finance-seven.store/user/recentproducts', { productId: productId });
-    console.log(data.data);
-    console.log('최근 본 상품 추가 완료');
-    return data;
+    const params = { productId: productId };
+    await authInstance.post('/user/recentproducts', params);
   } catch (err: any) {
     console.log(err.message);
   }
@@ -170,10 +80,9 @@ export const addRecentProduct = async (productId: number) => {
 // 검색결과 조회
 export const getSearchResults = async (title: string, category: string, page: number) => {
   try {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${document.cookie.slice(12)}`;
-    const data = await axios.get(`http://finance-seven.store/search?title=${title}&category=${category}&page=${page}`);
-    console.log(data.data.resultData);
-    return data.data.resultData;
+    const { data } = await defaultInstance.get(`/search?title=${title}&category=${category}&page=${page}`);
+    console.log(data.resultData);
+    return data.resultData;
   } catch (err: any) {
     console.log(err.message);
   }
