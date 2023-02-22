a2c7ce4d786a3c9858329325ccf1f3d2aaf0d70c
eun0leee <eun0leee@gmail.com>
2023-02-22 14:08:34 +0900

    Feat : 검색창_최근본상품 api 연결 및 구현 완료

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,51 +1,56 @@
 import { useEffect, useState, useMemo } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
 import { BiHistory } from 'react-icons/Bi';
 import { IoChevronForwardOutline } from 'react-icons/io5';
+
 import SearchBox from './SearchComponents/SearchBox';
 import KeywordsList from './SearchComponents/KeywordsList';
 import { getRecentProduct } from '../../common/api/Api';
 
+interface StateProps {
+  productName: string;
+  productId: number;
+  category: string;
+}
+
 const Search = () => {
-  const [data, setData] = useState([]);
-  const [autoSave, setAutoSave] = useState(true);
+  const [recentProduct, setRecentProduct] = useState<StateProps | undefined>();
+  const [keywordAutoSave, setKeywordAutoSave] = useState(true);
 
   useEffect(() => {
-    const getSeverData = async () => {
-      try {
-        const json = await getRecentProduct();
-        setData(json);
-        return json;
-      } catch (err) {}
+    const getSeverRecentProductData = async () => {
+      const json = await getRecentProduct();
+      setRecentProduct(json.slice(-1)[0]);
     };
-    getSeverData();
+    getSeverRecentProductData();
   }, []);
-
-  console.log(data);
 
   return (
     <Container>
-      <SearchBox autoSave={autoSave} />
-      <RecentProducts>
-        <h4>최근에 자세히 봤던</h4>
-        <DetailLink to={`/detail/:category/:id`}>
-          <div>
-            <span className="history">
-              <BiHistory />
-            </span>
-            상품명
-          </div>
-          <div className="info">
-            상세정보
-            <span className="move">
-              <IoChevronForwardOutline />
-            </span>
-          </div>
-        </DetailLink>
-      </RecentProducts>
-      <RecentKeywords autoSave={autoSave} setAutoSave={setAutoSave}>
-        <KeywordsList />
+      <SearchBox keywordAutoSave={keywordAutoSave} />
+      {recentProduct && (
+        <RecentProducts>
+          <h4>최근에 자세히 봤던</h4>
+          <DetailLink to={`/detail/${recentProduct.category}/${recentProduct.productId}`}>
+            <div>
+              <span className="history">
+                <BiHistory />
+              </span>
+              {recentProduct.productName}
+            </div>
+            <div className="info">
+              상세정보
+              <span className="move">
+                <IoChevronForwardOutline />
+              </span>
+            </div>
+          </DetailLink>
+        </RecentProducts>
+      )}
+      <RecentKeywords>
+        <KeywordsList keywordAutoSave={keywordAutoSave} setKeywordAutoSave={setKeywordAutoSave} />
       </RecentKeywords>
     </Container>
   );
@@ -55,6 +60,7 @@ const Container = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  margin-top: 140px;
   padding-bottom: 30px;
   gap: 30px;
 
@@ -65,9 +71,7 @@ const Container = styled.main`
   }
 `;
 
-const RecentProducts = styled.section`
-  margin-top: 70px;
-`;
+const RecentProducts = styled.section``;
 
 const DetailLink = styled(Link)`
   display: flex;
