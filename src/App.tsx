ca0e90aa6109e5e23b7a7f0e408285fa7b4827dd
0nesan <0nesan.o8o@gmail.com>
2023-02-23 18:17:06 +0900

    Feat: App.js 로직 MainPage 이동

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,8 @@ import { getCart, getUserInfo } from './common/api/Api';
 import Header from './components/Header';
 import TabBar from './components/TabBar';
 import { setUserItems } from './store/userSlice';
-import { user } from './store/userSlice';
-import { ReducerType } from './store/store';
-
 // 전체 공통 적용
 const App = () => {
-  
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const getUserData = async () => {
-      if(document.cookie){
-        try {
-          const userData = await getUserInfo()
-          dispatch(setUserItems(userData));
-        }catch(err){
-          console.log(err)
-        }    
-      }
-    }
-    getUserData()
-    
-    const getCartData = async () => {
-      const data = await getCart();
-
-      dispatch(setCartItems(data!.resultData));
-    };
-    getCartData();
-  }, []);
 
   return (
     <>
