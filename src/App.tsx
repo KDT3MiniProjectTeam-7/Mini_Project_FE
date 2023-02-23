fae5f677bc863d1b6cfd86419e761181296cbb87
0nesan <0nesan.o8o@gmail.com>
2023-02-23 18:18:36 +0900

    Feat: fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useEffect } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCartItems } from './store/cartSlice';
 import { GlobalStyle } from './common/style/Style';
-import { getCart, getUserInfo } from './common/api/Api';
 import Header from './components/Header';
 import TabBar from './components/TabBar';
-import { setUserItems } from './store/userSlice';
 // 전체 공통 적용
 const App = () => {
 
